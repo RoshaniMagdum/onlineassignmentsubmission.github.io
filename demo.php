b7ea04a734b04de4aa69ae9acad1096e54b7eeba
RoshaniMagdum <magdumroshani00@gmail.com>
2023-05-16 18:28:46 +0530

    My Online assignment submission website

---
+++ demo.php
@@ -0,0 +1,21 @@
+<?php include 'fileslogic.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>File Upload</title>
+</head>
+<body>
+ 
+<form action="demo.php" method="post" enctype="multipart/form-data">
+    <label>Title</label>
+    <input type="text" name="title">
+    <label>File Upload</label>
+    <input type="File" name="file">
+    <button type="submit" name="submit">
+ 
+ 
+</form>
+ 
+</body>
+</html>
+
---
+++ fileslogic.php
@@ -0,0 +1,9 @@
+<?php
+   
+   $conn = mysqli_connect("localhost","root","","assign");
+   
+   if(isset($_POST['save']))
+   {
+	echo "button clicked";  
+   }
+?>
